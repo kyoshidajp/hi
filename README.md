9e6f2e13e8e91fe7189f290cde38a7e36b5786ce
Katsuhiko YOSHIDA <claddvd@gmail.com>
2022-02-27 18:11:04 +0900

    Add  `--post-to-slack` option

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # hi
-Greetings
+Greeting before you starting, taking a lunch break, or finishing work.
+
+## Examples
+
+```sh
+# Before starting to work
+% hi --time-slot morning
+🏢 おはようございます。 (09:10:28-18:10:28)
+
+# Before taking a lunch break
+% hi --time-slot morning
+🍱 休憩します。 (12:12:23-13:12:23)
+
+# Before finishing work
+♨️  お疲れさまでした。
+```
+
+## Post to Slack
+
+`--post-to-slack` option enable you to post message to your Slack channel.
+
+1. Create Slack Incoming WebHook in your workspace, and get the web hook url.
+1. Create `.env` in a directory where you run `hi` command.
+1. Run `hi` command without `--post-to-slack` for cheking the message.
+1. If the message is good, then run the command with `--post-to-slack`.
+
+Sample of `.env`.
+
+```sh
+SLACK_WEB_HOOK_URL=https://hooks.slack.com/services/xxx
+SLACK_CHANNEL=#channel-name
+SLACK_USER_NAME=bot-kyoshida
+SLACK_ICON_EMOJI=:bot-kyoshida:
+```
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod hi;
 
 use argh::FromArgs;
 use chrono::{DateTime, Local};
+use dotenv::dotenv;
 
 #[derive(FromArgs)]
 /// Arguments
@@ -9,6 +10,10 @@ struct Arg {
     /// an optional time slot
     #[argh(option, short = 't', default = "default_time_slot()")]
     time_slot: String,
+
+    /// whether or not to post to slack (default: No)
+    #[argh(switch)]
+    post_to_slack: bool,
 }
 
 fn default_time_slot() -> String {
@@ -18,11 +23,14 @@ fn default_time_slot() -> String {
 fn main() {
     let arg: Arg = argh::from_env();
     let slot = arg.time_slot;
+    let post_to_slack = arg.post_to_slack;
 
-    do_hi(slot);
+    dotenv().ok();
+
+    do_hi(slot, post_to_slack);
 }
 
-fn do_hi(slot: String) {
+fn do_hi(slot: String, post_to_slack: bool) {
     let now: DateTime<Local> = Local::now();
     let message = match &*slot {
         "morning" => hi::morning(now),
@@ -32,4 +40,8 @@ fn do_hi(slot: String) {
     };
 
     println!("{}", message);
+
+    if post_to_slack {
+        hi::post_to_slack(message);
+    }
 }
